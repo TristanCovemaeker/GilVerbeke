4f34be7c91cc62bc956185fe631cf1b384eff5a0
Tristan Covemaeker <tristan.covemaeker@student.howest.be>
2021-01-29 18:58:52 +0100

    added button activation on checkbox

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,20 @@
+// General functions
 function playAudio() {
     var audio = new Audio('/audio/Pre_test_Tone.mp3');
     audio.play();
+}
+
+function toPage(page) {
+    window.location = page;
+}
+
+function disableButton() {
+    var checkbox = document.getElementById('consent');
+    var button = document.getElementById('button');
+
+    if (checkbox.checked == true) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
 }
