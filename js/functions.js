ab0138805c7cf82c2daada963373fe93a244bb6f
Tristan Covemaeker <tristan.covemaeker@student.howest.be>
2021-02-01 16:14:24 +0100

    added randomizeSounds function

--- js/functions.js
+++ js/functions.js
@@ -18,3 +18,30 @@ function disableButton() {
         button.disabled = true;
     }
 }
+
+function randomizeSounds() {
+    pre_test_stim_random = [];
+    var i = 0;
+
+    var random = Math.floor(Math.random() * pre_test_stim.length);
+    pre_test_stim_random.push(pre_test_stim[random]);
+    pre_test_stim.splice(random, 1);
+
+    while (pre_test_stim.length > 0) {
+        random = Math.floor(Math.random() * pre_test_stim.length);
+        var str = pre_test_stim[random].target;
+        var pos = str.indexOf('_');
+        var str_current = str.slice(0, pos);
+
+        str = pre_test_stim_random[i].target;
+        pos = str.indexOf('_');
+        var str_previous = str.slice(0, pos);
+
+        if (str_current == str_previous) {
+        } else {
+            pre_test_stim_random.push(pre_test_stim[random]);
+            pre_test_stim.splice(random, 1);
+            i++;
+        }
+    }
+}
