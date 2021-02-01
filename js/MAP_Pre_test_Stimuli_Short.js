222949f3431b2e1ce9558fa347d85843a93266b0
Tristan Covemaeker <tristan.covemaeker@student.howest.be>
2021-02-01 16:14:39 +0100

    changed path

--- js/MAP_Pre_test_Stimuli_Short.js
+++ js/MAP_Pre_test_Stimuli_Short.js
@@ -3,21 +3,21 @@ var pre_test_stim = [
     {
       target: "bink_1",
       probe: "Korte 'i' <b>(f)</b> of lange 'i' <b>(j)</b>",
-      target_audio: "/audio/CW_i_BINK_1.mp3",
+      target_audio: "/GilVerbeke/audio/CW_i_BINK_1.mp3",
       condition: "non-gender",
       task_part: "pre_test"
     },
     {
       target: "bink_3",
       probe: "Korte 'i' <b>(f)</b> of lange 'i' <b>(j)</b>",
-      target_audio: "/audio/CW_i_BINK_3.mp3",
+      target_audio: "/GilVerbeke/audio/CW_i_BINK_3.mp3",
       condition: "non-gender",
       task_part: "pre_test"   
     },
     {
       target: "bink_4",
       probe: "Korte 'i' <b>(f)</b> of lange 'i' <b>(j)</b>",
-      target_audio: "/audio/CW_i_BINK_4.mp3",
+      target_audio: "/GilVerbeke/audio/CW_i_BINK_4.mp3",
       condition: "non-gender",
       task_part: "pre_test"     
     }
